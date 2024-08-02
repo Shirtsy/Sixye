215e86751193319d31089894e697ebcb6a1d8ccb
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2024-08-02 14:02:29 -0400

    Update basalt_test.lua

--- basalt_test.lua
+++ basalt_test.lua
@@ -2,45 +2,54 @@ local basalt = require("dependencies.basalt")
 local get_point_canvas = require("dependencies.hex_render")
 local hexLookup = require("dependencies.hex_lookup")
 
-local function get_iota_type(iota)
-    local lua_type = type(iota)
-    if lua_type == "number" or lua_type == "boolean" or lua_type == "string"  then
-        return lua_type
-    elseif iota.null then
-        return "null"
-    elseif iota.garbage then
-        return "garbage"
-    elseif iota.x then
-        return "vector"
-    elseif iota.uuid then
-        return "entity"
-    elseif iota.startDir then
-        return "pattern"
-    elseif iota.iotaType then
-        return "iota_type"
-    elseif iota.entityType then
-        return "entity_type"
-    elseif iota.gate then
-        return "gate"
-    elseif iota.moteUuid then
-        return "mote"
-    elseif iota.itemType then
-        return "item_type"
-    else
-        return "list"
-    end
-end
-
 local function get_running_path()
     local runningProgram = shell.getRunningProgram()
     local programName = fs.getName(runningProgram)
     return runningProgram:sub( 1, #runningProgram - #programName )
 end
 
+local iota_types = {
+    number = "number",
+    boolean = "boolean",
+    string = "string",
+    null = "null",
+    garbage = "garbage",
+    vector = "x",
+    entity = "uuid",
+    pattern = "startDir",
+    iota_type = "iotaType",
+    entity_type = "entityType",
+    gate = "gate",
+    mote = "moteUuid",
+    item_type = "item_type",
+    list = 1,
+}
+
+local function get_iota_type(iota)
+    local lua_type = type(iota)
+    for k, v in pairs(iota_types) do
+        if v == lua_type then
+            return k
+        elseif type(iota) == "table" and iota[v] then
+            return k
+        end
+    end
+end
+
+local function get_iota_text(iota)
+    local iota_type = get_iota_type(iota)
+end
+
 local lookup = hexLookup.new(get_running_path() .. "dependencies/symbol-registry.json")
 local focus = require("dummy_focus")
 
-local main = basalt.createFrame():setTheme({FrameBG = colors.purple, FrameFG = colors.black})
+local main = basalt.createFrame()
+
+local right_menu = main:addScrollableFrame():setSize(20, 17):setPosition(27, 2)
+
+    local aLabel = right_menu:addLabel():setPosition(2, 2):setSize(18, 1)
+    aLabel:setText("Select an Iota")
+
 local hex_list = main:addList():setSize(24, 17):setPosition(2, 2)
 for i = 1, #focus do
     local iota_name = get_iota_type(focus[i])
